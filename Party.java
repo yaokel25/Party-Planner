9ba85281f8d25be861a0a2aefa7ecbf22167136a
yaokel25 <114490360+yaokel25@users.noreply.github.com>
2023-11-29 22:31:39 -0500

    Party.java

--- Party.java
+++ Party.java
@@ -1,19 +1,22 @@
+/**
+*Keliang Yao
+*This is the party class. It has methods to parse text file and load data onto the ArrayList guestList while also removing extra people. It will also make a 2d array of Attende objects of guests sorted by company. It has a printGuest method that is not used in Tester class of final project because it is only used to check my work. It also has a manual register method to gets user input of Attendee objects and adds them to guestList while also removing extra people. 
+**/
+
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
-import java.util.ArrayList;
+import java.util.ArrayList;//import ArrayList class
+
 public class Party{
 Scanner scan = new Scanner(System.in);
-public int[][] table = new int[10][10];
-public static int numCompanies = 16;
-public static int numRegister = 90;
-public static int totalNumGuest = 100;
-public int[] company = new int[16];
-public ArrayList<Attendee> guestList = new ArrayList<Attendee>();
+private int numCompanies = 16;
+private int numRegister = 90;
+private int totalNumGuest = 100;
+public int[] company = new int[16];//arrayList to keep track of # of people from each company
+public ArrayList<Attendee> guestList = new ArrayList<Attendee>();//ArrayList of Attendee objects 
 public static String[] companies = {"Wal-Mart", "Kroger", "Amazon", "Lowes", "Best Western", "KMart", "Fusian", "Heinz", "Gucci", "Prada", "Nike", "Dodge", "Maserati", "Razor", "AMD", "Razer"};
-public void parseData(){//parse data from downloaded file and upload them to arrayList guestList
-//String[] myArray;
-//ArrayList<Attendee> guestList = new ArrayList<Attendee>();
+public void parseData(){//parse data from downloaded file and upload them to arrayList guestList-also checks for and removes extra people
  try {
      File myObj = new File("partyguests (1).txt");
      Scanner myReader = new Scanner(myObj);
@@ -24,12 +27,12 @@ public void parseData(){//parse data from downloaded file and upload them to arr
        String iniName1 = myArray[1];//getting last name
        String iniName2 = myArray[2]; //getting first name
        int iniCompany = Integer.parseInt(myArray[3]) - 1;//get int for company and set company 1 to 0
-       int iniTable = 0;
-       int iniSeat = 0;
+       int iniTable = 0;//placeholder for table #
+       int iniSeat = 0;//placeholder for seat #
        Attendee guest = new Attendee(iniId, iniName1, iniName2, iniCompany, iniTable, iniSeat);
-       if(getCompany(iniCompany) == true){
+       if(getCompany(iniCompany) == true){//if there are less than 10 people from company iniCompany
          guestList.add(guest);
-       }
+       }//if
      }//while
      myReader.close();
    } catch (FileNotFoundException e) {
@@ -38,7 +41,7 @@ public void parseData(){//parse data from downloaded file and upload them to arr
    }//catch
    //return (guestList);
 }//parseData
-public boolean getCompany(int j){
+public boolean getCompany(int j){//return true if adding the person to the company will not exceed 10 people/company
 	if(company[j]+1 <= 10)
 	{
 		company[j]++;
@@ -46,164 +49,36 @@ public boolean getCompany(int j){
 	}
 	return false;
 }
+public void makeCompany(int holdCompany, ArrayList<ArrayList<Attendee>> company){//make 2d arrayList of attendee objects sorted by company
+  for(int i = 0; i < guestList.size(); i++){
+    if(guestList.get(i).getCompany() == holdCompany){//if attendee if from company      company.get(holdCompany).add(guestList.get(i));
+    }
+  }
+}
  
-
-
 public void printGuest(ArrayList<Attendee> guestList){//print guestList
  for(int i = 0; i < guestList.size(); i++){
    System.out.println(guestList.get(i));
  }
 }//printGuest  
-
-
-public int setCompanyNum(ArrayList<Attendee> guestList, int company){//check number of employees are preregistered for given company
- boolean filler = false;
- int counter = 0;
- for(int j = 0; j < guestList.size(); j++){
-   if(guestList.get(j).getCompany() == company){
-     counter++;
-     //guestList.set(counter, guestList.get(j));
-   }//if
-   else{
-     filler = true;
-   }//else
- }//for
-   return (counter);
-}//setCompanyNum
-
-
-public int setCompanyMem(int company, int index){//initialize companyMulti with the member's id
-   if(guestList.get(index).getCompany() == company){
-     return(guestList.get(index).getID());
-   }
-   else{
-     return (-1);
-   }
-}//setCompanyNum
-
-
-public ArrayList<Attendee> removePeople(ArrayList<Company> company, ArrayList<ArrayList<Integer>> companyMulti){//remove extra people starting with the smallest id number from that company
- boolean filler = true;
- for(int k = 0; k < numCompanies; k++){
-   if(company.get(k).getCompanyNum() > 10){
-     int extraPeople = company.get(k).getCompanyNum() - 10;
-     companyMulti.get(k).remove(0);//remove extra registered person's id from array of company members
-     //int counter = 0;
-     System.out.println("Sorry there are more than 10 people from " + companies[k] + ". " + extraPeople + " people are automatically removed.");
-     for(int i = 0; i < extraPeople; i++){//remove extra people from guestList starting with the smallest id
-       for(int h = 0; h < guestList.size(); h++){
-         if(guestList.get(h).getCompany() == k){
-           //counter++;//make sure only one object gets removed
-           guestList.remove(h);
-           company.get(k).removeMember();
-           break;
-         }//if
-         else{
-           filler = false;
-         }//else
-       }//for-h
-     }//for-i
-   }//if
-   else{
-     filler = true;
-   }
- }
- return(guestList);
-
-
-}//removePeople
-
-/*
-public void placePeople(ArrayList<ArrayList<Integer>> companyMulti, int company, ArrayList<Attendee> guestList){//place people into tables and seats
-  boolean filler = false;
- int tableNum = 0;
- int seatNum = 0;
- for(int i = 0; i < companyMulti.get(company).size(); i++){
-   if(tableNum > 9){
-     seatNum++;
-     tableNum = 0;
-   }
-   else{
-    filler = true;
-   }
-   table[tableNum][seatNum] = companyMulti.get(company).get(i);
-   int index = 0;
-   for(int y = 0; y < guestList.size(); y++){
-     if(guestList.get(y).getID() == companyMulti.get(company).get(i)){
-       index = y;
-       break;
-     }
-     else{
-      filler = false;
-     }
-   }
-   guestList.get(index).setTable(tableNum);
-   guestList.get(index).setSeat(seatNum);
-   tableNum++;
- }
-}//placePeople
-	*/
-public void manualRegister(){
- int registeredMem = guestList.size();
+public void manualRegister(){//manual register attendees after preregistered list is loaded into guestList
+ int registeredMem = guestList.size();//get # of people preregistered
  int stillNeed = totalNumGuest - registeredMem;
  System.out.println("There are " + registeredMem + " people preregistered. You can manually register " + stillNeed + " people.");
  System.out.println("Enter attendee ID in numerical order starting with 90 (so 1st manually registered guest is 90, 2nd is 91, etc): ");
- int iniId = scan.nextInt();
+ int iniId = scan.nextInt();//getting id
  System.out.println("Enter attendee last name: ");
- String iniName1 = scan.nextLine();
+ String iniName1 = scan.nextLine();//getting last name
  scan.nextLine();
  System.out.println("Enter attendee first name: ");
- String iniName2 = scan.nextLine();
+ String iniName2 = scan.nextLine();//getting first name
  //scan.nextLine();
  System.out.println("Enter attendee's company ID (it sould be a number): ");
- int iniCompany = scan.nextInt() - 1;
+ int iniCompany = scan.nextInt() - 1;//getting company id
  scan.nextLine();
  Attendee manualA = new Attendee(iniId, iniName1, iniName2, iniCompany, 0, 0);
- guestList.add(manualA);
+  if(getCompany(iniCompany) == true){//if there are less than 10 people in company iniCompany
+     guestList.add(manualA);
+   }//if
 }//manualRegister
-public String printRoster1(ArrayList<ArrayList<Integer>> companyMulti, int company, int index){//gets roster of tables and seats with inputted company
-int numTables = 10;
-int numSeats = 10;
-int findTable = 0;
-int findSeat = 0;
-   for(int j = 0; j < numTables; j++){
-     for(int a = 0; a < numSeats; a++){
-       if(table[j][a] == companyMulti.get(company).get(index)){
-         findTable = j;
-         findSeat = a;
-       }
-     }
-   }
-   return("table: " + findTable + " seat: " + findSeat + " ");
- }//printRoster1
-public Attendee printRoster2(int table){//gets roster by table with imputted table
- int numTables = 10;
- int numSeats = 10;
- int getInfo = 0;
- for(int i = 0; i < guestList.size(); i++){
-   if(guestList.get(i).getTable() == table){
-     getInfo = i;
-     break;
-   }
- }
- return(guestList.get(getInfo));
- }//printRoster2
-public Attendee getPerson(int id){//get member's info with inputted id 
-Attendee a1 = new Attendee(0, "0", "0", 0, 0, 0);
- for(int i = 0; i < guestList.size(); i++){
-   if(guestList.get(i).getID() == id){
-     a1 = guestList.get(i);
-     break;
-   }
- }
- return(a1);
-}
-
-	public void setTable(int[][] setTable)
-	{
-		table = setTable;
-	}
-
-
-
 }//party
